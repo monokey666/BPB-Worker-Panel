fd7c3798b3855f4ab2a495775147b8bb9639fcf1
monokey666 <monokey443@gmail.com>
2024-07-26 17:26:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '85c5efcb-2657-49fa-8b47-35851688016b';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
@@ -193,13 +193,13 @@ export default {
                         return new Response('Success', {
                             status: 200,
                             headers: {
-                                'Set-Cookie': 'jwtToken=; Path=/; Secure; SameSite=None; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
+                                'Set-Cookie': 'jwtToken=; Path=/; Secure; SameSite None; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
                                 'Content-Type': 'text/plain',
                             }
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
+                          return new Response('Not found', { status: 404 });
                         url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
